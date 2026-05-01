733c6489e2ecb13549f42e43b3f608270a49d5a1
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-05-01 20:57:28 +0900

    feat: mount status mode panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { StatusPanel } from './components/StatusPanel';
 import './styles.css';
 import './phase2.css';
+import './phase25.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <section className="appShell boundaryShell">
+      <div className="panel statusModePanel">
+        <StatusPanel />
+      </div>
+    </section>
   </React.StrictMode>,
 );
