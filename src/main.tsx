7143c0f4905f1aef2b25a92074522ac3675888d2
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-05-01 20:44:50 +0900

    feat: load phase 2 styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './phase2.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
